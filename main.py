3afad504d86e8b852361f7197ce61ddce187268d
Tomasz Macałka <tomasz.macalka@student.pk.edu.pl>
2022-01-02 23:15:40 +0100

    Przygotowanie docelowego ukladu przyciskow, przeniesienie panelu do klasy, utworzenie metod dla zakupu i wyboru produktu

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 from decimal import *
 
 possible_coins = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5]
-input_panel = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 
 def decimal_2places_rounded(value_before):
@@ -96,53 +95,73 @@ class ItemStorage:
             if money_placed >= selected_item_price:
                 print("Zakup " + selected_item_name + " udany")
                 print("Wydaje reszte " + str(money_placed - selected_item_price))
+            else:
+                print("Za malo srodkow, produkt kosztuje " + str(selected_item_price))
 
 
-class BuyersChoice:
+# class BuyersChoice:
+#     def __init__(self):
+#         self.__choice = 0
+#
+#     def get_choice(self):
+#         return self.__choice
+#
+#     def check_digits(self):
+#         return len(str(self.__choice))
+#
+#     def add_digit(self, sent_digit):
+#         if len(str(self.__choice)) == 0 or self.__choice == 0:
+#             self.__choice = int(str(sent_digit))
+#         elif (len(str(self.__choice))) == 1:
+#             self.__choice = int(str(self.__choice) + str(sent_digit))
+
+
+class MachinePanel:
     def __init__(self):
-        self.__choice = 0
+        # Tworzenie okna
+        self._window = Tk()
+        self._window.title("Automat")
+        self._money_amount = StringVar()
+        self._item_choice = StringVar()
+        self._buyers_choice = ""
+        # Tworzenie siatki na przyciski
+        self._mainframe = ttk.Frame(self._window)
+        # Umieszczenie siatki w oknie
+        self._mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        # Dodanie przycisków do wrzucania monet
+        self._i = 0
+        for coin in possible_coins:
+            ttk.Button(self._mainframe, text="Wrzuć " + str(coin) + "zł",
+                       command=lambda lcoin=coin: self.action_on_money(lcoin)).grid(column=4, row=self._i)
+            self._i += 1
+        [ttk.Button(self._mainframe, text=str(digit + 1),
+                    command=lambda ldigit=digit: self.action_on_choice(ldigit + 1)).grid(row=digit // 3 + 2,
+                                                                                         column=digit % 3) for digit
+         in range(9)]
+        ttk.Button(self._mainframe, text="0", command=lambda: self.action_on_choice(0)).grid(row=5, column=1)
+        # Dodanie przycisku sprawdzenia wartości zawartości
+        ttk.Button(self._mainframe, text="Przerwij", command=lambda: machine_coins.return_coins()).grid(column=0, row=5)
+        ttk.Label(self._mainframe, textvariable=self._money_amount).grid(column=0, row=0)
+        ttk.Label(self._mainframe, textvariable=self._item_choice).grid(column=0, row=1)
+        self._window.mainloop()
 
-    def get_choice(self):
-        return self.__choice
+    def action_on_choice(self, choice):
+        choice = str(choice)
+        if len(self._buyers_choice) == 0 or len(self._buyers_choice) == 2:
+            self._buyers_choice = choice
+            self._item_choice.set(self._buyers_choice)
+        elif len(self._buyers_choice) == 1:
+            self._buyers_choice += choice
+            self._item_choice.set(self._buyers_choice)
+            machine.buy_item(int(self._buyers_choice), machine_coins.coin_sum())
 
-    def check_digits(self):
-        return len(str(self.__choice))
-
-    def add_digit(self, sent_digit):
-        if len(str(self.__choice)) == 0 or self.__choice == 0:
-            self.__choice = int(str(sent_digit))
-        elif (len(str(self.__choice))) == 1:
-            self.__choice = int(str(self.__choice) + str(sent_digit))
+    def action_on_money(self, money):
+        machine_coins.add_coin(Coin(money))
+        self._money_amount.set(machine_coins.coin_sum())
 
 
 pepsi = Item("Pepsi", 30, 3.50, 5)
-buyers_choice = BuyersChoice()
 machine = ItemStorage()
 machine.add_item(pepsi)
 machine_coins = CoinStorage()
-# Tworzenie okna
-window = Tk()
-window.title("Automat")
-# Tworzenie siatki na przyciski
-mainframe = ttk.Frame(window)
-# Umieszczenie siatki w oknie
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-# Dodanie przycisków do wrzucania monet
-i = 0
-chosen_product = 0
-for coin in possible_coins:
-    ttk.Button(mainframe, text="Wrzuć " + str(coin) + "zł",
-               command=lambda coin=coin: machine_coins.add_coin(Coin(coin))).grid(column=2, row=i)
-    i += 1
-for input_button in input_panel:
-    ttk.Button(mainframe, text=str(input_button),
-               command=lambda input_button=input_button: buyers_choice.add_digit(input_button)).grid(
-        column=3, row=input_button)
-# Dodanie przycisku sprawdzenia wartości zawartości
-ttk.Button(mainframe, text="Przerwij", command=lambda: print(machine_coins.coin_sum())).grid(column=1, row=0)
-ttk.Button(mainframe, text="Zwroc", command=lambda: machine_coins.return_coins()).grid(column=1, row=1)
-ttk.Button(mainframe, text="Zakup",
-           command=lambda: machine.buy_item(buyers_choice.get_choice(), machine_coins.coin_sum())).grid(column=1,
-                                                                                                        row=2)
-ttk.Button(mainframe, text="Zamowienie", command=lambda: print(buyers_choice.get_choice())).grid(column=1, row=3)
-window.mainloop()
+display = MachinePanel()
