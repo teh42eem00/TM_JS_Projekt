739d514601c87c1aded336aa3226414b4bb4af99
Tomasz Macałka <tomasz.macalka@vsoft.pl>
2022-01-14 17:19:14 +0100

    dokonczenie mechanizmu wydawania reszty oraz mechanizmu transferu pieniedzy miedzy klientem a automatem

--- main.py
+++ main.py
@@ -5,6 +5,27 @@ from decimal import *
 possible_coins = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0]
 
 
+def check_arrays(money_to_give, rest_available):
+    counter = 0
+    temp = rest_available.copy()
+    for money in money_to_give:
+        print("Trzeba wydac:", money)
+        if money in rest_available:
+            rest_available.remove(money)
+            counter += 1
+            print("Posiadane monety do wydania reszty: ", rest_available)
+            print("Licznik: ", counter)
+
+    if counter == len(money_to_give):
+        print("Jest ok, mozna wydac reszte")
+        print("Nowa tablica z pieniedzmi do reszty: ", rest_available)
+        return True
+    else:
+        print("Brak mozliwosci wydania reszty!")
+        print("W zwiazku z tym stara tablica: ", temp)
+        return False
+
+
 def decimal_2places_rounded(value_before):
     return Decimal(round(value_before, 2))
 
@@ -18,8 +39,9 @@ def return_change(to_return, coins=None):
             return c
         if c < to_return:
             flag = c
-    temp_balance = round(to_return - flag, 2)
-    return [flag] + [return_change(temp_balance)]
+    if flag is not None:
+        temp_balance = round(to_return - flag, 2)
+        return [flag] + [return_change(temp_balance)]
 
 
 def flatten(nested_list):
@@ -51,11 +73,33 @@ class CoinStorage:
         else:
             print("Przeslany obiekt nie jest moneta!")
 
+    def return_array_of_value(self):
+        value_list = [float(o.get_value()) for o in self.__coin_list]
+        return value_list
+
+    def return_rest(self, rest_coin_value):
+        selected_coin = next(
+            (i for i, item in enumerate(self.__coin_list) if item.get_value() == rest_coin_value), -1)
+        rest_coin_txt = "Wydaje reszte o wartosci " + str(self.__coin_list.pop(selected_coin).get_value()) + "zl."
+        return rest_coin_txt
+
     def return_coins(self):
         returned_coins_txt = ""
         while len(self.__coin_list):
             returned_coins_txt += "Zwracam monete o wartosci " + str(self.__coin_list.pop().get_value()) + " zl.\n"
         return returned_coins_txt
+
+    def pop_coin_object(self):
+        return self.__coin_list.pop()
+
+    def return_len(self):
+        return len(self.__coin_list)
+
+    def take_money_inside(self, customer_money):
+        while customer_money.return_len():
+            self.__coin_list.append(customer_money.pop_coin_object())
+        print(self.coin_sum())
+        print(self.return_array_of_value())
 
     def coin_sum(self):
         counted_coins = 0
@@ -111,19 +155,44 @@ class ItemStorage:
 
     def get_item_price(self, chosen_item_number):
         if 50 >= chosen_item_number >= 30:
-            selected_item = next(
-                (i for i, item in enumerate(self.__item_list) if item.get_number() == chosen_item_number), -1)
+            selected_item = 0
+            for index, item in enumerate(self.__item_list):
+                if item.get_number() == chosen_item_number:
+                    selected_item = index
             selected_item_price = self.__item_list[selected_item].get_price()
             return selected_item_price
+        else:
+            print("Blad!")
 
     def buy_item(self, chosen_item_number, money_placed):
         selected_item_price = self.get_item_price(chosen_item_number)
         if money_placed >= selected_item_price:
-            print("Zakup udany")
-            rest = float(money_placed - selected_item_price)
-            print("Wydaje reszte " + str(return_change(rest)))
-        else:
-            print("Za malo srodkow, produkt kosztuje " + str(selected_item_price))
+            rest = money_placed - selected_item_price
+            print("Obliczona reszta do wydania: ", rest)
+            if rest != 0:
+                rest_list = return_change(rest, machine_rest_coins.return_array_of_value())
+                print("Reszta bedzie wydana monetami ", rest_list)
+
+                if rest_list is not list and rest_list in machine_rest_coins.return_array_of_value():
+                    messagebox.showinfo("Sukces", "Zakup produktu o numerze " + str(chosen_item_number) + " udany")
+                    messagebox.showinfo("Reszta", "Wydaje reszte:\n" + str(machine_rest_coins.return_rest(rest_list)))
+                    machine_rest_coins.take_money_inside(machine_coins)
+                elif rest_list is None:
+                    messagebox.showinfo("Nie mozna wydac reszty", "zakup anulowany!")
+                    messagebox.showinfo("Zwrot monet", machine_coins.return_coins())
+                elif check_arrays(rest_list, machine_rest_coins.return_array_of_value()):
+                    rest_list = list(flatten(rest_list))
+                    rest_text = ""
+                    for elem in rest_list:
+                        rest_text += machine_rest_coins.return_rest(elem)
+                    messagebox.showinfo("Reszta", "Wydaje reszte:\n" + str(rest_text))
+                    machine_rest_coins.take_money_inside(machine_coins)
+                else:
+                    messagebox.showinfo("Nie mozna wydac reszty", "zakup anulowany!")
+                    messagebox.showinfo("Zwrot monet", machine_coins.return_coins())
+            else:
+                messagebox.showinfo("Sukces", "Zakup produktu o numerze " + str(chosen_item_number) + " udany")
+                machine_rest_coins.take_money_inside(machine_coins)
 
 
 class MachinePanel:
@@ -137,7 +206,7 @@ class MachinePanel:
         # Tworzenie siatki na przyciski
         self._mainframe = ttk.Frame(self._window)
         # Umieszczenie siatki w oknie
-        self._mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self._mainframe.grid(column=0, row=0)
         # Dodanie przycisków do wrzucania monet
         self._i = 0
         for coin in possible_coins:
@@ -151,6 +220,10 @@ class MachinePanel:
         ttk.Button(self._mainframe, text="0", command=lambda: self.action_on_choice(0)).grid(row=5, column=1)
         # Dodanie przycisku sprawdzenia wartości zawartości
         ttk.Button(self._mainframe, text="Przerwij", command=lambda: self.action_on_cancel()).grid(column=0, row=5)
+        ttk.Button(self._mainframe, text="Monety - Reszta",
+                   command=lambda: print(machine_rest_coins.return_array_of_value())).grid(column=0, row=6)
+        ttk.Button(self._mainframe, text="Monety - Wplacone",
+                   command=lambda: print(machine_coins.return_array_of_value())).grid(column=0, row=7)
         ttk.Label(self._mainframe, textvariable=self._money_amount).grid(column=0, row=0)
         ttk.Label(self._mainframe, textvariable=self._item_choice).grid(column=0, row=1)
         self._window.mainloop()
@@ -163,22 +236,36 @@ class MachinePanel:
         elif len(self._buyers_choice) == 1:
             self._buyers_choice += choice
             self._item_choice.set(self._buyers_choice)
-            if machine_coins.coin_sum() < machine.get_item_price(int(self._buyers_choice)):
-                messagebox.showinfo("Koszt wynosi", str(machine.get_item_price(int(self._buyers_choice))))
+            if 50 >= int(self._buyers_choice) >= 30:
+                if machine_coins.coin_sum() < machine.get_item_price(int(self._buyers_choice)):
+                    messagebox.showinfo("Za mało!",
+                                        "Produkt kosztuje " + str(machine.get_item_price(int(self._buyers_choice))))
+                else:
+                    self._money_amount.set("")
+                    machine.buy_item(int(self._buyers_choice), machine_coins.coin_sum())
             else:
-                machine.buy_item(int(self._buyers_choice), machine_coins.coin_sum())
+                messagebox.showinfo("Blad", "Bledny numer produktu!")
 
     def action_on_money(self, money):
         machine_coins.add_coin(Coin(money))
-        self._money_amount.set(machine_coins.coin_sum())
+        self._money_amount.set(str(machine_coins.coin_sum()))
 
     def action_on_cancel(self):
         messagebox.showinfo("Zwrot monet", machine_coins.return_coins())
         self._money_amount.set("")
 
 
-pepsi = Item("Pepsi", 30, 3.50, 5)
+pepsi = Item("Pepsi", 30, 3, 5)
+sok = Item("Sok pomaranczowy", 48, 3.5, 5)
+woda_gaz = Item("Woda gazowana", 49, 2.5, 5)
+woda_niegaz = Item("Woda niegazowana", 50, 2, 5)
 machine = ItemStorage()
 machine.add_item(pepsi)
+machine.add_item(sok)
+machine.add_item(woda_gaz)
+machine.add_item(woda_niegaz)
 machine_coins = CoinStorage()
+machine_rest_coins = CoinStorage()
+machine_rest_coins.add_coin(Coin(2))
+machine_rest_coins.add_coin(Coin(5))
 display = MachinePanel()
