92d46d3b360cbd8d04308acb197fff289fa29f4f
Tomasz Macałka <tomasz.macalka@vsoft.pl>
2022-01-16 16:35:15 +0100

    rozbudowanie komentarzy, ustawienie wszystkich atrybutow na prywatne, import towarow z pliku

--- automat.py
+++ automat.py
@@ -1,18 +1,19 @@
-import interface
-import logic
+import interface  # import modulu odpowiadajacego za interfejs
+import logic  # import modulu odpowiadajacego za logike
 
-pepsi = logic.Item("Pepsi", 30, 3, 0)
-mala_tania_woda = logic.Item("Mala tania woda", 47, 1, 5)
-sok = logic.Item("Sok pomaranczowy", 48, 3.5, 5)
-woda_gaz = logic.Item("Woda gazowana", 49, 2.5, 5)
-woda_niegaz = logic.Item("Woda niegazowana", 50, 2, 5)
-machine = logic.ItemStorage()
-machine.add_item(pepsi)
-machine.add_item(mala_tania_woda)
-machine.add_item(sok)
-machine.add_item(woda_gaz)
-machine.add_item(woda_niegaz)
+machine = logic.ItemStorage()  # utworzenie automatu przechowujacego przedmioty klasy Item
+
+# pobranie towarow z pliku i zaladowanie ich do automatu (jednolinijkowy odczyt pliku)
+for product in [line.strip() for line in open("towary.txt")]:
+    name, number, price, count = product.split(',')
+    machine.add_item(logic.Item(name, int(number), logic.decimal_2places_rounded(price), int(count)))
+
+# utworzenie kontenerow na pieniadze umieszczane przez klienta oraz na pieniadze wydawane jako reszta
 machine_coins = logic.CoinStorage()
 machine_rest_coins = logic.CoinStorage()
-machine_rest_coins.add_multiple_coins(1, 5)
+
+# dodanie pieniedzy do kontenera na wydawanie reszty list comprehension
+[machine_rest_coins.add_multiple_coins(value, 1) for value in logic.possible_coins]
+
+# uruchomienie interfejsu graficznego
 display = interface.MachinePanel(machine, machine_coins, machine_rest_coins)
--- logic.py
+++ logic.py
@@ -1,6 +1,59 @@
-from decimal import *
+from decimal import *  # import decimal
 
 possible_coins = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0]  # dozwolone nominaly
+
+
+# Wyjatki dla tego modulu
+class LogicError(Exception):
+    """Klasa bazowa dla zglaszanych w tym module wyjatkow"""
+    pass
+
+
+class NoChangeAvailableError(LogicError):
+    """Wyjatek wywolywany gdy brak pieniedzy do wydania reszty
+
+    atrybuty:
+    message_title - tytul wiadomosci zwracanej w okienku
+    message_content - tresc wiadomosci zwracanej w okienku"""
+
+    def __init__(self, message_title, message_content):
+        self.message_title = message_title
+        self.message_content = message_content
+
+    def __str__(self):
+        return self.message_title, self.message_content
+
+
+class NoProductOnStockError(LogicError):
+    """Wyjatek wywolywany gdy brak towaru (rozni sie od tego z interfejsu, ze jest wywolywany gdy klient juz wrzucil
+    odpowiednia kwote i automat rozpoczal proces zakupu)
+
+    atrybuty:
+    message_title - tytul wiadomosci zwracanej w okienku
+    message_content - tresc wiadomosci zwracanej w okienku"""
+
+    def __init__(self, message_title, message_content):
+        self.message_title = message_title
+        self.message_content = message_content
+
+    def __str__(self):
+        return self.message_title, self.message_content
+
+
+class InvalidObjectTypeError(LogicError):
+    """Wyjatek wywolywany gdy przedmiot umieszczany w kontenerze ma nieprawidlowy typ (np. w przypadku kontenerow
+    ItemStorage, CoinStorage oczekiwane sa klasy Item, Coin
+
+    atrybuty:
+    message_title - tytul wiadomosci zwracanej w okienku
+    message_content - tresc wiadomosci zwracanej w okienku"""
+
+    def __init__(self, message, object_type):
+        self._message = message
+        self._object_type = object_type
+
+    def __str__(self):
+        return self._message, self._object_type
 
 
 def decimal_2places_rounded(value_before):
@@ -8,7 +61,8 @@ def decimal_2places_rounded(value_before):
 
     argumenty:
     value_before - wartosc przed zamiana"""
-    return Decimal(round(value_before, 2))
+    value_before = Decimal(value_before)
+    return round(value_before, 2)
 
 
 def unpack_dict(value, count):
@@ -17,7 +71,8 @@ def unpack_dict(value, count):
     argumenty:
     value - pierwszy argument slownika - wartosc monet
     count - drugi argument slownika - ilosc monet danej wartosci"""
-    return value, count
+    # powrotna konwersja z groszy na zł
+    return decimal_2places_rounded(value / 100), count
 
 
 def return_change(coins, to_return, coin_index=0):
@@ -62,23 +117,23 @@ class Coin(Thing):
     """Klasa definiujaca monete.
 
     argumenty:
-    value - wartosc monety (musi byc w liscie possible coins)"""
+    value - wartosc monety"""
 
     def __init__(self, value):
-        if value in possible_coins:
-            self.__value = decimal_2places_rounded(value)
-        else:
-            self.__value = 0
+        # konwersja na decimal, 2 miejsca po przecinku
+        self._value = decimal_2places_rounded(value)
 
     def get_value(self):
         """Metoda zwracajaca zaokraglona do dwoch miejsc po przecinku wartosc monety (typu Decimal)"""
-        return decimal_2places_rounded(self.__value)
+        # pobranie wartosci decimal, 2 miejsca po przecinku
+        return decimal_2places_rounded(self._value)
 
 
 class CoinStorage(Storage):
     """Klasa przechowujaca monety. Monety sa przechowywane w liscie."""
 
     def __init__(self):
+        # lista przechowujaca monety
         self._coin_list = []
 
     def add_coin(self, added_coin):
@@ -87,10 +142,14 @@ class CoinStorage(Storage):
         argumenty:
 
         added_coin - obiekt klasy Moneta, ktory chcemy dodac do kontenera"""
-        if isinstance(added_coin, Coin):
-            self._coin_list.append(added_coin)
-        else:
-            print("Przeslany obiekt nie jest moneta!")
+        try:
+            # jezeli moneta jest klasy Coin to dodaj, w przeciwnym wypadku wyjatek
+            if isinstance(added_coin, Coin):
+                self._coin_list.append(added_coin)
+            else:
+                raise InvalidObjectTypeError("Przeslany obiekt nie jest moneta! Jest on typu ", type(added_coin))
+        except InvalidObjectTypeError as inv_type:
+            print(*inv_type.__str__())
 
     def add_multiple_coins(self, coin_value, coin_count):
         """Metoda sluzaca do hurtowego dodania wielu monet danego nominalu.
@@ -98,6 +157,7 @@ class CoinStorage(Storage):
         argumenty:
         coin_value - nominal monety
         coin_count - liczba monet"""
+        # dodaj okreslona ilosc monet o okreslonym nominale
         for ccount in range(coin_count):
             self._coin_list.append(Coin(coin_value))
 
@@ -106,12 +166,13 @@ class CoinStorage(Storage):
 
         argumenty:
         chosen_coin_value - wartosc poszukiwanej monety"""
-        return next(i for i, coin in enumerate(self._coin_list) if
-                    coin.get_value() == chosen_coin_value)  # generator expression
+        # wyrazenie generujace zwracajace index pierwszej napotkanej monety o danej wartosci
+        return next(i for i, coin in enumerate(self._coin_list) if coin.get_value() == chosen_coin_value)
 
     def return_array_of_value(self):
         """Metoda zwracajaca liste skladajaca sie tylko z wartosci wszystkich przechowywanych przez kontener monet"""
-        value_list = [float(o.get_value()) for o in self._coin_list]  # list comprehension
+        # list comprehension zwracajace liste wartosci wszystkich monet
+        value_list = [float(o.get_value()) for o in self._coin_list]
         return value_list
 
     def return_rest(self, rest_coin_value, rest_coin_count):
@@ -123,8 +184,9 @@ class CoinStorage(Storage):
         rest_coin_value - wartosc monety do zwrocenia
         rest_coin_count - ilosc monet o danej wartosci do zwrocenia"""
         rest_coin_txt = ""
+        # dla podanej ilosci sciaga z listy monety o okreslonym nominale i dodaje ich wartosc do zwracanego komunikatu
         for rcount in range(rest_coin_count):
-            selected_coin_index = self.get_coin_index(rest_coin_value / 100)  # powrotna konwersja z groszy na zł
+            selected_coin_index = self.get_coin_index(rest_coin_value)
             rest_coin_txt += "Wydaje reszte o wartosci " + str(
                 self._coin_list.pop(selected_coin_index).get_value()) + "zl.\n"
         return rest_coin_txt
@@ -133,20 +195,24 @@ class CoinStorage(Storage):
         """Metoda zwracajaca wszystkie monety w danym kontenerze. Wywolywana w momencie klikniecia przez uzytkownika przerwij.
         Wykonuje pop do momentu oproznienia listy. Zwraca tekst z informacje o tym jakie monety zostaly zwrocone."""
         returned_coins_txt = ""
+        # dopoki lista nie jest pusta sciaga z listy wszystkie monety i zwraca ich wartosc w komunikacie
         while len(self._coin_list):
             returned_coins_txt += "Zwracam monete o wartosci " + str(self.pop_coin_object().get_value()) + " zl.\n"
         return returned_coins_txt
 
     def pop_coin_object(self):
         """Metoda zdejmujaca z listy ostatnia monete, zwraca obiekt typu Moneta."""
+        # zdejmij z listy ostatni obiekt
         return self._coin_list.pop()
 
     def return_len(self):
         """Metoda zwracajaca dlugosc listy przechowujacej monety."""
+        # zwroc dlugosc listy przechowujacej monety
         return len(self._coin_list)
 
     def check_empty_coin_list(self):
         """Metoda zwracajaca typ bool (True/False). Zwraca true gdy lista jest pusta, false gdy lista nie jest pusta."""
+        # jezeli lista monet jest pusta zwroc True, w przeciwnym wypadku false
         if not self._coin_list:
             return True
         else:
@@ -159,6 +225,7 @@ class CoinStorage(Storage):
 
         argumenty:
         customer_money - obiekt typu CoinStorage z ktorego maja zostac przeniesione wszystkie pieniadze"""
+        # dopoki kontener z pieniedzmi klienta nie jest pusty pobieraj monety i dodawaj je do kontenera automatu
         while customer_money.return_len():
             self._coin_list.append(customer_money.pop_coin_object())
 
@@ -166,7 +233,8 @@ class CoinStorage(Storage):
         """Metoda, ktora za pomoca wyrazenia generujacego zwraca sume wartosci wszystkich monet w kontenerze. Uzywana
         min. do sprawdzania czy kwota wplacona przez uzytkownika jest poprawna lub do wypisywania wartosci wplaconych monet
          na wyswietlaczu."""
-        return sum(c.get_value() for c in self._coin_list)  # wyrazenie generujace
+        # wyrazenie generujace zwracajace sume wartosci wszystkich monet
+        return sum(c.get_value() for c in self._coin_list)
 
 
 class Item(Thing):
@@ -222,10 +290,14 @@ class ItemStorage(Storage):
 
         argumenty:
         added_item - obiekt typu Item dodawany do listy"""
-        if isinstance(added_item, Item):
-            self._item_list.append(added_item)
-        else:
-            print("Przeslany obiekt nie jest przedmiotem!")
+        try:
+            # jezeli przedmiot jest klasy Item to dodaj go do listy, w przeciwnym wypadku wyjatek
+            if isinstance(added_item, Item):
+                self._item_list.append(added_item)
+            else:
+                raise InvalidObjectTypeError("Przeslany obiekt nie jest przedmiotem! Jest on typu ", type(added_item))
+        except InvalidObjectTypeError as inv_type:
+            print(*inv_type.__str__())
 
     def get_item(self, chosen_item_number):
         """Metoda przeszukujaca przedmioty za pomoca generator expression w celu znalezienia towaru o podanym numerze.
@@ -233,13 +305,15 @@ class ItemStorage(Storage):
 
         argumenty:
         chosen_item_number - zadany numer produktu, ktorego poszukujemy"""
-        return next(item for item in self._item_list if item.get_number() == chosen_item_number)  # generator expression
+        # wyrazenie generujace, zwracajace pierwszy napotkany przedmiot o danym kodzie towaru
+        return next(item for item in self._item_list if item.get_number() == chosen_item_number)
 
     def get_item_price(self, chosen_item_number):
         """Metoda zwracajaca cene towaru o podanym numerze
 
         argumenty:
         chosen_item_number - numer produktu, ktorego cene chcemy poznac"""
+        # zwroc cene towaru o wskazanym kodzie towaru
         item_obj = self.get_item(chosen_item_number)
         return item_obj.get_price()
 
@@ -248,6 +322,7 @@ class ItemStorage(Storage):
 
         argumenty:
         chosen_item_number - numer produktu, ktorego ilosc chcemy zmniejszyc o 1"""
+        # zmniejsz ilosc towaru
         self.get_item(chosen_item_number).lower_count()
 
     def buy_item(self, chosen_item_number, money_placed, machine_rest_coins, machine_coins):
@@ -261,39 +336,54 @@ class ItemStorage(Storage):
         money_placed - kwota wplacona przez uzytkownika
         machine_rest_coins - obiekt typu CoinStorage przechowujacy pieniadze do wydawania reszty
         machine_coins - obiekty typu CoinStorage przechowujacy wplacone przez klienta pieniadze"""
-        selected_item_count = self.get_item(chosen_item_number).check_item_count()
-        if selected_item_count:
-            selected_item_price = self.get_item_price(chosen_item_number)
-            rest = money_placed - selected_item_price
-            if rest == 0:
-                machine_rest_coins.take_money_inside(machine_coins)
-                self.return_item(chosen_item_number)
-                return "Sukces", "Zakup produktu o numerze " + str(chosen_item_number) + " udany"
-            else:
-                rest_available_coins = machine_rest_coins.return_array_of_value()  # lista wartosci
-                rest_available_coins = [int(i * 100) for i in rest_available_coins]  # konwersja na grosze
-                counted_rest_coins = {}  # slownik przechowujacy monety w formie {wartosc , ilosc}
-                counted_rest_coins_list = list()  # lista przechowujaca slowniki zliczonych monet
+        try:
+            # sprawdzenie stanu danego towaru
+            selected_item_count = self.get_item(chosen_item_number).check_item_count()
+            # jezeli towar na stanie
+            if selected_item_count:
+                # pobierz cene
+                selected_item_price = self.get_item_price(chosen_item_number)
+                # oblicz reszte
+                rest = money_placed - selected_item_price
+                if rest == 0:
+                    # jezeli brak reszty zabierz pieniadze od klienta i wydaj towar
+                    machine_rest_coins.take_money_inside(machine_coins)
+                    self.return_item(chosen_item_number)
+                    # zwrot komunikatu do messageboxa
+                    return "Sukces", "Zakup produktu o numerze " + str(chosen_item_number) + " udany"
+                else:
+                    # list comprehension - lista dostepnych monet do wydania reszty, zmiana na grosze, sortowanie
+                    rest_available_coins = sorted([int(i * 100) for i in machine_rest_coins.return_array_of_value()],
+                                                  reverse=True)
+                    counted_rest_coins = {}  # slownik przechowujacy monety w formie {wartosc , ilosc}
+                    counted_rest_coins_list = list()  # lista przechowujaca slowniki zliczonych monet
 
-                for r in rest_available_coins:  # zliczenie ilosci monet dostepnych do wydania reszty
-                    counted_rest_coins[r] = counted_rest_coins.get(r, 0) + 1
+                    for r in rest_available_coins:  # zliczenie ilosci monet dostepnych do wydania reszty
+                        counted_rest_coins[r] = counted_rest_coins.get(r, 0) + 1
 
-                for key, (d_value, d_count) in enumerate(counted_rest_coins.items()):  # dodanie slownikow do listy
-                    d = {"value": d_value, "count": d_count}
-                    counted_rest_coins_list.append(d)
+                    for key, (d_value, d_count) in enumerate(counted_rest_coins.items()):  # dodanie slownikow do listy
+                        d = {"value": d_value, "count": d_count}
+                        counted_rest_coins_list.append(d)
 
-                returned_change_dict_list = return_change(counted_rest_coins_list, int(rest * 100))  # obliczenie reszty
-                if returned_change_dict_list is None:  # jezeli nie udalo sie obliczyc reszty zwroc blad
-                    return "Brak reszty!", "Tylko odliczona kwota!\n"  # + str(machine_coins.return_coins())
-                else:  # w przeciwnym razie pobierz pieniadze i wydaj reszte
-                    change_txt = ""
-
-                    for rc in returned_change_dict_list:  # dla wszystkich slownikow reszty (nominal, ilosc)
-                        change_txt += machine_rest_coins.return_rest(
-                            *unpack_dict(**rc))  # zwroc reszte z rozpakowanego slownika
+                    returned_change_dict_list = return_change(counted_rest_coins_list, int(rest * 100))  # licz reszte
+                    if returned_change_dict_list is None:  # jezeli nie udalo sie obliczyc reszty zglos wyjatek
+                        raise NoChangeAvailableError("Brak reszty!", "Tylko odliczona kwota!\n")
+                    else:  # w przeciwnym razie pobierz pieniadze i wydaj reszte
+                        change_txt = ""
+                        for rc in returned_change_dict_list:  # dla wszystkich slownikow reszty (nominal, ilosc)
+                            # zwroc reszte z rozpakowanego slownika
+                            change_txt += machine_rest_coins.return_rest(*unpack_dict(**rc))
+                        # zabranie pieniedzy od klienta i umieszczenie w kontenerze na wydawanie reszty
                         machine_rest_coins.take_money_inside(machine_coins)
+                        # wydanie przedmiotu, zabranie jednej sztuki
                         self.return_item(chosen_item_number)
-                    return "Sukces", (
-                            "Zakup produktu o numerze " + str(chosen_item_number) + " udany\n" + str(change_txt))
-        else:
-            return "Brak towaru", "Brak towaru w automacie"
+                        # zwrot komunikatu do messageboxa
+                        return "Sukces", ("Zakup produktu o numerze " + str(chosen_item_number) +
+                                          " udany\n" + str(change_txt))
+            else:
+                raise NoProductOnStockError("Brak towaru", "Brak towaru w automacie")
+            # obsluga wyjatkow
+        except NoChangeAvailableError as no_change:
+            return no_change.message_title, no_change.message_content
+        except NoProductOnStockError as no_product:
+            return no_product.message_title, no_product.message_content
